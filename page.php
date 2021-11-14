33637f2c0069310ba0ee5c1833dc5767f12b3824
HandiSutriyan <handisutrian@gmail.com>
2021-11-15 05:59:22 +0700

    add basic php

--- page.php
+++ page.php
@@ -1,7 +1,8 @@
 <?php
 while(have_posts()) {
     the_post(); ?>
-    <h2><a href="<?php the_permalink(  ) ?>"><?php the_title() ?></a></h2>
+    <h2>Ini Haaman Page bukan Post </h2>
+    <h2><?php the_title() ?></h2>
     <p><?php the_content() ?></p>
     <?php  }
 ?>
